b8610ca3128dfaf38105765ab24cf1f1cf5bb1a2
Foyjur Rafee <frnaib9@gmail.com>
2026-08-16 16:05:46 +0600

    userdashboard page created

---
+++ resources/views/frontend/userdashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.frontendLayout')
+@section('title')
+Homepage
+@endsection
+@section('content')
+<aside>
+
+</aside>
+<div class="col-lg-6">
+    <img src="{{ assets('/frontend/img/user.png') }}" alt="">
+    <h1></h1>
+</div>
+
+@endsection

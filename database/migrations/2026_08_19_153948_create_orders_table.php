2781050a2de804e74ad9597b5ed9e7580c4865ee
abdur <abdur@gmail.com>
2026-08-19 22:03:37 +0600

    SSL Commerce

---
+++ database/migrations/2026_08_19_153948_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->float('amount')->default(10);
+            $table->string('address');
+            $table->string('status')->nullable();
+            $table->string('currency')->default('BDT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,14 @@ use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\SslCommerzPaymentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomepageController::class, 'home'])->name('frontend.index');
-Route::get('/shop', [ShopController::class, 'shop'])->name('frontend.shop');  
-Route::get('/live-search', [ShopController::class, 'liveSearch'])->name('frontend.search');  
+Route::get('/shop', [ShopController::class, 'shop'])->name('frontend.shop');
+Route::get('/live-search', [ShopController::class, 'liveSearch'])->name('frontend.search');
 
 
 Route::get('/contact', [contactController::class, 'contact'])->name('frontend.contact');
@@ -23,12 +24,26 @@ Route::get('/pages', [PagesController::class, 'error'])->name('frontend.error-pa
 
 
 // Add to Cart
-Route::get('/cart/{id}', [ShopController::class,'addToCart'])->name('product.cart')->middleware('cus');
+Route::get('/cart/{id}', [ShopController::class, 'addToCart'])->name('product.cart')->middleware('cus');
 
 
 Route::get('/product/{id}', [HomepageController::class, 'productDetails'])
     ->name('frontend.product.details');
-  
+
+
+
+// SSLCOMMERZ Start
+Route::get('/checkout', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 // Admin Login Routes
 Auth::routes();
@@ -39,6 +54,3 @@ Auth::routes();
 
 // Admin Routes
 require __DIR__ . '/admin.php';
-
-
-
